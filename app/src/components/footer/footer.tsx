a194ea7c2b0fc38a2f045bed844b97ed9e7a539d
Upayan Mazumder <upayanm3@gmail.com>
2025-01-12 16:03:10 +0530

    Refactor global styles and footer component for improved aesthetics and functionality

--- app/src/components/footer/footer.tsx
+++ app/src/components/footer/footer.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { JSX } from "react";
+import Link from "next/link";
 import footerStyles from "./footer.module.css";
 import links from "../../data/links.json";
 import { FaGithub, FaDiscord, FaInstagram, FaFacebook, FaLinkedin, FaArrowUp } from "react-icons/fa";
@@ -21,24 +22,24 @@ const Footer: React.FC = () => {
     return (
         <footer className={footerStyles.footer}>
             <div className={footerStyles.footerTop}>
-                <div className={footerStyles.column}>
-                    <h3>Resources</h3>
+                <div id="footer-resources" className={footerStyles.column}>
+                    <Link href="#footer-resources"><h3>Resources</h3></Link>
                     <ul>
                         <li><a href="https://github.com/upayanmazumder/upayan.dev">Source Code</a></li>
                         <li><a href="/p/privacy-policy">Privacy Policy</a></li>
                         <li><a href="/p/terms-of-service">Terms of Service</a></li>
                     </ul>
                 </div>
-                <div className={footerStyles.column}>
-                    <h3>Companies</h3>
+                <div id="footer-companies" className={footerStyles.column}>
+                    <Link href="#footer-companies"><h3>Companies</h3></Link>
                     <ul>
                         <li><a href="https://bbn.one">BBN</a></li>
                         <li><a href="https://eranodes.com">EraNodes</a></li>
                         <li><a href="https://purbyte.com">Purbyte</a></li>
                     </ul>
                 </div>
-                <div className={footerStyles.column}>
-                    <h3>Projects</h3>
+                <div id="footer-projects" className={footerStyles.column}>
+                    <Link href="#footer-projects"><h3>Projects</h3></Link>
                     <ul>
                         <li><a href="https://www.boardly.in/">Boardly</a></li>
                         <li><a href="https://github.com/upayanmazumder/Collaborative-Article-Sharing">Collaborative Article Sharing</a></li>
@@ -49,11 +50,9 @@ const Footer: React.FC = () => {
             <div className={footerStyles.footerBottom}>
                 <ul className={footerStyles.footerLinks}>
                     {filteredLinks.map((link, index) => (
-                        <li key={index}>
-                            <a href={link.url} target="_blank" rel="noopener noreferrer">
-                                {iconDictionary[link.name]}
-                            </a>
-                        </li>
+                        <a key={index} href={link.url} target="_blank" rel="noopener">
+                            {iconDictionary[link.name]}
+                        </a>
                     ))}
                 </ul>
                 <ul className={footerStyles.toTop}>

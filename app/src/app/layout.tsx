d23006e533773a6005e6508d5d249ff0f7237eab
Upayan Mazumder <upayanm3@gmail.com>
2025-01-12 17:26:49 +0530

    Remove header component and associated styles to streamline layout

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "../components/header/header";
 import Sidebar from "../components/sidebar/sidebar";
 import Footer from "../components/footer/footer";
 
@@ -28,7 +27,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Header />
         <Sidebar />
         {children}
         <Footer />

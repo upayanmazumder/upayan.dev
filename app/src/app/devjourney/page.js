cd0cce80cc9081561f5ad5318598348e64075b39
Upayan Mazumder <upayanm3@gmail.com>
2025-01-13 20:04:58 +0530

    Add DevJourney component and syntax highlighting styles for enhanced coding journey presentation

---
+++ app/src/app/devjourney/page.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import DevJourney from '../../components/devjourney/devjourney';
+
+const DevJourneyPage = () => {
+  return (
+    <main>
+      <DevJourney />
+    </main>
+  );
+};
+
+export default DevJourneyPage;
+
+export const metadata = {
+  title: "Upayan - Devjourney",
+  description: "View my coding journey",
+};

ad336cba6a2a8e5299888a774cbbbe9ae1659c2e
Upayan Mazumder <upayanm3@gmail.com>
2025-02-01 22:17:26 +0530

    ADDED PWA!

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -27,6 +27,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Sidebar />
         <Nav />

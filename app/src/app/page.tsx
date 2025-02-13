2842ada58c10b09e306587bf2577881668b9a35b
Upayan Mazumder <upayanm3@gmail.com>
2025-02-13 23:42:50 +0530

    Refactor hero component layout and styles for improved design consistency

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,12 +1,10 @@
 import Hero from "../components/hero/hero";
-import Activity from "../components/activity/activity";
 import Projects from "../components/projects/projects";
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      <Activity />
       <Projects />
     </main>
   );

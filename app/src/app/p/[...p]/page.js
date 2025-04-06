36bb2cbe65ad5c4bf72acaccccf40cb634c367c6
Upayan Mazumder <upayanm3@gmail.com>
2025-04-06 14:35:54 +0530

    Add new page and GetRekt component; enhance project card styling

---
+++ app/src/app/p/[...p]/page.js
@@ -0,0 +1,17 @@
+import React from "react";
+import GetRekt from "../../../components/getrekt/getrekt";
+
+const P = () => {
+  return (
+    <main>
+      <GetRekt />
+    </main>
+  );
+};
+
+export default P;
+
+export const metadata = {
+  title: "Upayan - Blah Blah",
+  description: "View my coding journey",
+};
---
+++ app/src/components/getrekt/getrekt.tsx
@@ -0,0 +1,25 @@
+export default function GetRekt() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        color: "rgb(229 229 229 / 79%)",
+        textAlign: "center",
+      }}
+    >
+      <h1>Get Rekt!!!</h1>
+      <p style={{ fontSize: "1.5rem" }}>
+        You really expect a page here? I mean, come on! It&apos;s just a simple
+        website. But hey, if you want to know what I do with your data,
+        here&apos;s the deal: I don&apos;t collect any personal information from
+        you. No cookies, no tracking, no funny business. Just pure coding
+        goodness. If you have any questions or concerns, feel free to reach out.
+        Otherwise, enjoy browsing my site without any worries. Cheers!
+      </p>
+    </div>
+  );
+}

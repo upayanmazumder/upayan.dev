f85ddef8eab044be01f96d7a6f42f6aba755d1f9
Upayan M <upayanm3@gmail.com>
2025-05-02 21:20:22 +0530

    refactor: remove unused useRouter import from Header component

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,14 +1,7 @@
 import React from "react";
 import Hero from "../components/hero/Hero";
-import dynamic from "next/dynamic";
-
-const Clubs = dynamic(() => import("../components/clubs/Clubs"), {
-  ssr: true,
-});
-
-const Projects = dynamic(() => import("../components/projects/Projects"), {
-  ssr: true,
-});
+import Clubs from "../components/clubs/Clubs";
+import Projects from "../components/projects/Projects";
 
 export default function Home() {
   return (

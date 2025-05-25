b027cb07f7bc019bd6c336934f8092d6ee25c37c
Upayan M <upayanm3@gmail.com>
2025-05-26 01:38:04 +0530

    Add Medical Roster component and styling; update TypeScript config

---
+++ app/src/app/medical-roster/page.tsx
@@ -0,0 +1,9 @@
+import MedicalRoster from "../../components/medical-roster/medical-roster";
+
+export default function MedicalRosterPage() {
+  return (
+    <main>
+      <MedicalRoster />
+    </main>
+  );
+}

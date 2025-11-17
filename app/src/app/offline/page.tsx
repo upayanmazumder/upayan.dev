2d44200e3cbec858ebb4728b88fa5e257c4a8f2e
Upayan <upayanm3@gmail.com>
2025-11-17 21:17:28 +0530

    Delete old

--- app/src/app/offline/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function OfflinePage() {
-  return (
-    <main>
-      <h1>You are offline 😓</h1>
-      <p>This page is being served from the service worker cache.</p>
-    </main>
-  );
-}
